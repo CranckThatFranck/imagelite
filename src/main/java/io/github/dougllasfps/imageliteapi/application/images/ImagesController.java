ffebea59bcbd1c706feb3d022a814e4f8c4ae3f8
CranckThatFranck <franck.oliver@outlook.com.br>
2026-01-30 16:18:24 -0300

    First real push

--- src/main/java/io/github/dougllasfps/imageliteapi/application/images/ImagesController.java
+++ src/main/java/io/github/dougllasfps/imageliteapi/application/images/ImagesController.java
@@ -1,4 +1,33 @@
 package io.github.dougllasfps.imageliteapi.application.images;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/v1/images")
+@Slf4j //Faz que o lombok adicione log
 public class ImagesController {
+
+    //*
+    //  {"name": "", "size":100 } //application/json
+
+    // mult-part/form-data
+    //*
+
+    @PostMapping
+    public ResponseEntity save(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("name") String name,
+            @RequestParam("tags") String tags
+            ){
+        log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
+        log.info("Nome definido para a imagem: {}", name);
+        log.info("Tags definidas para a imagem: {}", tags);
+        return ResponseEntity.ok().build();
+    }
 }
